adf68e32ad38f68e50f3cef659baa1c5e5de7226
Michiel Missotten <michiel@lil-web.fr>
2013-04-02 14:24:36 +0200

    Ajout de la table sur les tacheS.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -8,7 +8,7 @@ CREATE TABLE `projets` (
     `description` text,
     `apikey` char(32) NOT NULL DEFAULT '',
     PRIMARY KEY (`id_projet`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE `utilisateurs` (
     `id_utilisateur` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,9 +18,27 @@ CREATE TABLE `utilisateurs` (
     `motdepasse` varchar(80) NOT NULL DEFAULT '',
     `apikey` char(32) NOT NULL DEFAULT '',
     PRIMARY KEY (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+
+CREATE TABLE `taches` (
+    `id_tache` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `nom` varchar(80) NOT NULL DEFAULT '',
+    `description` text,
+    `date_creation` int(11) NOT NULL,
+    `statut` int(1) NOT NULL DEFAULT '0',
+    `id_projet` int(10) unsigned DEFAULT NULL,
+    `id_utilisateur` int(11) unsigned DEFAULT NULL,
+    PRIMARY KEY (`id_tache`),
+    KEY `fk_taches_utilisateurs` (`id_utilisateur`),
+    KEY `fk_taches_projets` (`id_projet`),
+    CONSTRAINT `fk_taches_projets` FOREIGN KEY (`id_projet`) 
+        REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_taches_utilisateurs` FOREIGN KEY (`id_utilisateur`) 
+        REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 # --- !Downs
 
 DROP TABLE `projets`;
 DROP TABLE `utilisateurs`;
+DROP TABLE `taches`;
