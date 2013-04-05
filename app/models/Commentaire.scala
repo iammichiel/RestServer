9a1ed18833166c5623634023993d432fbcc1e540
Michiel Missotten <michiel@lil-web.fr>
2013-04-06 00:02:01 +0200

    Listing des commentaires.

---
+++ app/models/Commentaire.scala
@@ -0,0 +1,58 @@
+package models
+
+import scala.language.postfixOps
+
+import anorm._
+import anorm.SqlParser._
+
+import play.api._
+import play.api.db._
+import play.api.Play.current
+import play.api.libs.json.Json
+
+import org.joda.time.DateTime
+
+case class Commentaire(
+    id: Pk[Long] = NotAssigned, 
+    utilisateur: Long, 
+    dateCreation: DateTime, 
+    contenu: String 
+) {
+    def toJson = Json.toJson(Map(
+        "id"           -> id.toString, 
+        "utilisateur"  -> utilisateur.toString, 
+        "dateCreation" -> dateCreation.getMillis.toString,
+        "contenu"      -> contenu
+    ))
+}
+
+object Commentaire {
+    
+   val simple = {
+        get[Pk[Long]]("id_commentaire") ~
+        get[Long]("id_utilisateur") ~
+        get[Long]("date_creation") ~
+        get[String]("contenu") map {
+            case id~utilisateur~dateCreation~contenu => 
+                Commentaire(id, utilisateur, new DateTime(dateCreation), contenu)   
+        }
+    }
+
+    def all(idTache: String, apikey: String) = {
+        DB.withConnection { implicit connection =>
+            SQL(
+                """
+                    SELECT 
+                        id_commentaire, id_utilisateur, date_creation, contenu
+                    FROM 
+                        taches 
+                    WHERE 
+                        id_tache = {idTache} AND apikey = {apikey}
+                """
+            ).on(
+                'idTache -> idTache,
+                'apikey  -> apikey
+            ).as(simple *)
+        }
+    }
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -21,21 +21,37 @@ CREATE TABLE `utilisateurs` (
 );
 
 CREATE TABLE `taches` (
-  `id_tache` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `titre` varchar(80) NOT NULL DEFAULT '',
-  `description` text,
-  `date_creation` bigint(20) NOT NULL,
-  `statut` int(1) NOT NULL DEFAULT '0',
-  `id_projet` int(10) unsigned DEFAULT NULL,
-  `id_utilisateur` int(11) unsigned DEFAULT NULL,
-  `apikey` char(32) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_tache`),
-  KEY `fk_taches_utilisateurs` (`id_utilisateur`),
-  KEY `fk_taches_projets` (`id_projet`),
-  CONSTRAINT `fk_taches_projets` FOREIGN KEY (`id_projet`) 
-    REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_taches_utilisateurs` FOREIGN KEY (`id_utilisateur`) 
-    REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE
+    `id_tache` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `titre` varchar(80) NOT NULL DEFAULT '',
+    `description` text,
+    `date_creation` bigint(20) NOT NULL,
+    `statut` int(1) NOT NULL DEFAULT '0',
+    `id_projet` int(10) unsigned DEFAULT NULL,
+    `id_utilisateur` int(11) unsigned DEFAULT NULL,
+    `apikey` char(32) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id_tache`),
+    KEY `fk_taches_utilisateurs` (`id_utilisateur`),
+    KEY `fk_taches_projets` (`id_projet`),
+    CONSTRAINT `fk_taches_projets` FOREIGN KEY (`id_projet`) 
+        REFERENCES `projets` (`id_projet`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_taches_utilisateurs` FOREIGN KEY (`id_utilisateur`) 
+        REFERENCES `utilisateurs` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `commentaires` (
+    `id_commentaire` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `id_tache` int(11) unsigned NOT NULL,
+    `id_utilisateur` int(11) unsigned NOT NULL,
+    `date_creation` bigint(20) NOT NULL,
+    `contenu` text NOT NULL,
+    `apikey` char(32) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id_commentaire`),
+    KEY `fk_commentaires_taches` (`id_tache`),
+    KEY `fk_commentaires_utilisateurs` (`id_utilisateur`),
+    CONSTRAINT `fk_commentaires_utilisateurs` FOREIGN KEY (`id_utilisateur`) 
+        REFERENCES `utilisateurs` (`id_utilisateur`),
+    CONSTRAINT `fk_commentaires_taches` FOREIGN KEY (`id_tache`) 
+        REFERENCES `taches` (`id_tache`)
 );
 
 # --- !Downs
@@ -43,3 +59,4 @@ CREATE TABLE `taches` (
 DROP TABLE `projets`;
 DROP TABLE `utilisateurs`;
 DROP TABLE `taches`;
+DROP TABLE `commentaires`;
